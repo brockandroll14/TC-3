4a77db3f5f6f76ccf0df7c28888f7f1295ee90f8
brockandroll14 <145859541+brockandroll14@users.noreply.github.com>
2023-11-03 17:41:25 -0500

    PotentialFinalCommit

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,12 +1,14 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Data,",p:["p1"]},{w:"Documents",p:["p1"]},{w:"&",p:["p1"]},{w:"Files",p:["p1","p2"]},{w:"Google",p:["p2","p3"]},{w:"Drive",p:["p2"]},{w:"and",p:["p2","p3"]},{w:"Folders",p:["p2","p3"]},{w:"Drives",p:["p3"]},{w:"Jadu",p:["p4","p5"]},{w:"CXM",p:["p4"]},{w:"Workflows",p:["p4"]},{w:"XFP",p:["p5"]},{w:"Online",p:["p5"]},{w:"Forms",p:["p5"]},{w:"Step",p:["p5"]},{w:"by",p:["p5"]},{w:"Unify",p:["p6"]},{w:"Tableau",p:["p6"]},{w:"Connector",p:["p6"]}];
+var keywords=[{w:"Box",p:["p0"]},{w:"Secure",p:["p0"]},{w:"Storage",p:["p0"]},{w:"Data,",p:["p1"]},{w:"Documents",p:["p1"]},{w:"&",p:["p1"]},{w:"Files",p:["p1","p8"]},{w:"Google",p:["p2","p3","p8"]},{w:"Drive",p:["p2","p8"]},{w:"Drives",p:["p3"]},{w:"and",p:["p3","p8"]},{w:"Folders",p:["p3","p8"]},{w:"Jadu",p:["p4","p5"]},{w:"CXM",p:["p4"]},{w:"Workflows",p:["p4"]},{w:"XFP",p:["p5"]},{w:"Online",p:["p5"]},{w:"Forms",p:["p5"]},{w:"Step",p:["p5"]},{w:"by",p:["p5"]},{w:"Unify",p:["p6"]},{w:"Tableau",p:["p6"]},{w:"Connector",p:["p6"]},{w:"Context",p:["p7"]},{w:"Sensitive",p:["p7"]},{w:"Help",p:["p7"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6];
-ph["p2"]=[7, 8, 6, 9, 10];
-ph["p3"]=[7, 11, 9, 10];
+ph["p2"]=[7, 8];
+ph["p3"]=[7, 9, 10, 11];
 ph["p4"]=[12, 13, 14];
 ph["p5"]=[12, 15, 16, 17, 18, 19, 18];
 ph["p6"]=[20, 21, 22];
+ph["p7"]=[23, 24, 25];
+ph["p8"]=[7, 8, 6, 10, 11];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
---
+++ out/docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+var keywords=[{w:"Box",p:["p0"]},{w:"Secure",p:["p0"]},{w:"Storage",p:["p0"]},{w:"Data,",p:["p1"]},{w:"Documents",p:["p1"]},{w:"&",p:["p1"]},{w:"Files",p:["p1"]},{w:"Google",p:["p2","p3"]},{w:"Drive",p:["p2"]},{w:"Drives",p:["p3"]},{w:"and",p:["p3"]},{w:"Folders",p:["p3"]},{w:"Jadu",p:["p4","p5"]},{w:"CXM",p:["p4"]},{w:"Workflows",p:["p4"]},{w:"XFP",p:["p5"]},{w:"Online",p:["p5"]},{w:"Forms",p:["p5"]},{w:"Step",p:["p5"]},{w:"by",p:["p5"]},{w:"Unify",p:["p6"]},{w:"Tableau",p:["p6"]},{w:"Connector",p:["p6"]},{w:"Context",p:["p7"]},{w:"Sensitive",p:["p7"]},{w:"Help",p:["p7"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7, 8];
+ph["p3"]=[7, 9, 10, 11];
+ph["p4"]=[12, 13, 14];
+ph["p5"]=[12, 15, 16, 17, 18, 19, 18];
+ph["p6"]=[20, 21, 22];
+ph["p7"]=[23, 24, 25];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
